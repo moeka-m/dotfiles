da11d30afc03f425c4ef69875ba81ddb2f6fe566
moeka-m <moeka.dev@gmail.com>
2019-11-03 16:56:54 +0900

    fix: make slink

--- deploy.sh
+++ deploy.sh
@@ -8,10 +8,9 @@ for f in .??*; do
     [ "$f" = ".git" ] && continue
     [ "$f" = ".gitignore" ] && continue
 
-    echo "$DOTPATH/$f" "$HOME"/"$f"
-    # ln -snfv "$DOTPATH/$f" "$HOME"/"$f"
+    ln -sfnv "$DOTPATH/$f" "$HOME"/"$f"
 done
 
-if [[ -d $HOME/.zsh/antigen ]]; then
+if [[ ! -d $HOME/.zsh/antigen ]]; then
     git clone https://github.com/zsh-users/antigen.git $HOME/.zsh/antigen
 fi
