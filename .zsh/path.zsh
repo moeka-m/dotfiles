86738550a812f1b0b3d971787e5c2c7c2cc72083
moeka-m <moeka.dev@gmail.com>
2020-02-02 14:34:10 +0900

    ignore Brewfile.lock.json

--- .zsh/path.zsh
+++ .zsh/path.zsh
@@ -1,11 +1,14 @@
 # pyenv
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
-export PATH="/usr/local/opt/openssl/bin:$PATH"
-export LDFLAGS="-L/usr/local/opt/openssl/lib"
-export CPPFLAGS="-I/usr/local/opt/openssl/include"
-eval "$(pyenv init -)"
+# export PYENV_ROOT="$HOME/.pyenv"
+# export PATH="$PYENV_ROOT/bin:$PATH"
+# export PATH="/usr/local/opt/openssl/bin:$PATH"
+# export LDFLAGS="-L/usr/local/opt/openssl/lib"
+# export CPPFLAGS="-I/usr/local/opt/openssl/include"
+# eval "$(pyenv init -)"
 
 # goenv
 export GOPATH="$HOME/ghq"
 export PATH="$PATH:$GOPATH/bin"
+
+# brew
+export HOMEBREW_BUNDLE_FILE="$HOME/.dotfiles/Brewfile"
