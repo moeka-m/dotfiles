987bd4215ef70e3af7082c308198d480ac952a0e
moeka-m <moeka.dev@gmail.com>
2021-12-19 09:21:18 +0900

    update

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -12,3 +12,7 @@ alias lcgen='leetcode show -gxl python3'
 # exercism-cli
 alias em='exercism'
 alias emsub='exercism submit'
+
+# brew doctorのワーニング回避
+# ref: https://www.xn--ebkc7kqd.com/entry/pyenv-brew-doctor
+alias brew="env PATH=${PATH//$(pyenv root)\/shims:/} brew"
--- .zsh/path.zsh
+++ .zsh/path.zsh
@@ -2,6 +2,9 @@
 # anyenv
 eval "$(anyenv init -)"
 
+# pyenv
+eval "$(pyenv init --path)"
+
 # go
 export GOENV_DISABLE_GOPATH=1
 export GOPATH="$HOME/ghq"
@@ -17,6 +20,6 @@ export PIPENV_VENV_IN_PROJECT=1 #project直下に仮想環境を作る
 ### brew
 export HOMEBREW_BUNDLE_FILE="$HOME/.dotfiles/Brewfile"
 
-### その時々で使いたいコマンド置き場
+### 臨時で使いたいコマンドは ~/bin 配下にシンボリックリンクを作って使う
 mkdir -p "$HOME/bin"
 export PATH="$HOME/bin:$PATH"
